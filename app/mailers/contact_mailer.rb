2128ae36f0ac870bb6f26721226986831eff2185
Espur <espen.taftoe@gmail.com>
2017-07-04 12:45:16 +0000

    fixed typo in registrations controller

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ActionMailer::Base
-    default to: 'espen.taftoe@gmail.com'
+    default to: 'mediespesialisten@gmail.com'
     
     def contact_email(name, email, body)
         @name = name
